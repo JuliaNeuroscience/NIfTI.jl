13b1a38f96e0f2508adf6f70dc84b6de6a090f9b
Simon Kornblith <simon@simonster.com>
2013-04-25 23:10:22 -0400

    Add argument to transform tensor to RAS (or any other orientation)

--- examples/dicom2nifti.jl
+++ examples/dicom2nifti.jl
@@ -33,6 +33,13 @@ function parse_commandline()
 		"--sphinx"
 			help = "correct for sphinx position (for monkeys)"
 			action = :store_true
+		"--axes"
+			help = """orientation of tensor in the resulting volume. This is a three-character label
+			that determines how the data is stored, and how it is displayed in applications that do
+			not make use of the orientation matrix in the NIfTI header. Valid characters are:
+			L (= left), R (= right), A (= anterior), P (= posterior), I (= inferior), and
+			S (= superior). If not specified, the orientation in the DICOM file is preserved."""
+			default = nothing
 		"dicomdir"
 			help = "directory of DICOM files"
 			required = true
@@ -68,6 +75,17 @@ end
 
 function main()
 	cmd = parse_commandline()
+
+	if cmd["axes"] == nothing
+		axes = nothing
+	else
+		axes = uppercase(cmd["axes"])
+		if length(axes) != 3 || contains(axes, 'R') + contains(axes, 'L') != 1 ||
+				contains(axes, 'A') + contains(axes, 'P') != 1 ||
+				contains(axes, 'S') + contains(axes, 'I') != 1
+			error("Invalid axes provided")
+		end
+	end
 
 	transform = cmd["sphinx"] ? [-1 0 0; 0 0 1; 0 -1 0] : [-1 0 0; 0 -1 0; 0 0 1]
 
@@ -110,7 +128,7 @@ function main()
 		time_step = lookup(d, (0x0018, 0x0080)) # This is the TR, but may not be the time step
 		phase_encoding_direction = lookup(d, (0x0018, 0x1312))
 
-		# Convert LPS orentation to RAS
+		# Convert orentation to RAS
 		orientation = transform*reshape(orientation, (3, 2))
 
 		# Determine Z as cross product of X and Y
@@ -133,6 +151,49 @@ function main()
 		raw = cat(3,
 			[findtag(slices[i], (0x7FE0, 0x0010), series_number, "Pixel Data")[1] for i in p]...)
 
+		orientation = float32(hcat(orientation, image_positions[:, p[1]]))
+
+		dim_info = phase_encoding_direction.data[1] == "ROW" ? (1, 2, 3) :
+				phase_encoding_direction.data[1] == "COL" ? (2, 1, 3) :
+				(0, 0, 0)
+
+		voxel_size = tuple(pixel_spacing..., slice_thickness)
+
+		# Permute according to axes specified on command line
+		if axes != nothing
+			# Determine permutation of current volume to RAS
+			ras = Array(Int, 3)
+			sign = Array(Bool, 3)
+			rg = [1:3]
+			abs_orientation = abs(orientation)
+			for i = 1:3
+				idx = findmax(abs_orientation[i, rg])[2]
+				ras[i] = delete!(rg, idx)
+				sign[i] = orientation[i, ras[i]] >= 0
+			end
+
+			# Determine permutation of RAS to user-preferred orientation
+			specified_axes = Int[(contains("RL", c) ? 1 : contains("AP", c) ? 2 : 3)
+				for c in axes]
+			specified_sign = Bool[contains("RAS", c) for c in axes]
+			output_axes = ras[specified_axes]
+
+			# Permute
+			raw = permutedims(raw, output_axes)
+			orientation[:, 1:3] = orientation[:, output_axes]
+			voxel_size = voxel_size[output_axes]
+			dim_info = dim_info[output_axes]
+
+			# Flip signs and dimensions
+			flip_sign = find(sign[specified_axes] .!= specified_sign)
+			orientation[:, flip_sign] = -orientation[:, flip_sign]
+			for dim in flip_sign
+				raw = flipdim(raw, dim)
+				orientation[dim, 4] += (sign[specified_axes[dim]] ? 1 : -1) * 
+					(size(raw, dim) - 1)*voxel_size[dim]
+			end
+		end
+
 		# Create a directory for each protocol
 		protocol_dir = joinpath(cmd["niftidir"], replace(protocol_name, "/", ""))
 		if !isdir(protocol_dir)
@@ -140,11 +201,8 @@ function main()
 		end
 
 		# Write NIfTI volumes
-		ni = NIfTIVolume(raw; voxel_size=tuple(pixel_spacing..., slice_thickness),
-			orientation=float32(hcat(orientation, image_positions[:, p[1]])),
-			dim_info=(phase_encoding_direction.data[1] == "ROW" ? (1, 2, 3) :
-				phase_encoding_direction.data[1] == "COL" ? (2, 1, 3) :
-				(0, 0, 0)),
+		ni = NIfTIVolume(raw; voxel_size=voxel_size,
+			orientation=orientation, dim_info=dim_info,
 			time_step=time_step != false ? time_step.data[1] : 0f0)
 		niftiwrite(joinpath(protocol_dir, "$(series_number).nii"), ni)
 	end
