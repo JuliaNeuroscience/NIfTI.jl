f91b31093664128fe2f909a0a04be7abf26c2dd8
Simon Kornblith <simon@simonster.com>
2014-06-22 21:50:56 -0400

    Remove old references to AGPL

--- examples/dicom2nifti.jl
+++ examples/dicom2nifti.jl
@@ -8,21 +8,6 @@
 # work with other scanners. It may give incorrect information. Test thoroughly
 # and use at your own risk.
 
-# Copyright (C) 2013   Simon Kornblith
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using DICOM, NIfTI, ArgParse
 
 function parse_commandline()
