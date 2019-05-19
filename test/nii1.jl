9d9125d14db77b673fe8547a58b86c946bb5c23f
Tokazama <zchristensen7@gmail.com>
2019-05-19 05:02:03 -0400

    First of many test commits
    Quick note that the change of '2000000' => '2000' is because the new
    version of NIfTI.jl does not change all dim4s to a common time unit

--- test/nii1.jl
+++ test/nii1.jl
@@ -22,24 +22,43 @@ for file_name in (NII, GZIPPED_NII, HDR, GZIPPED_HDR)
     @testset "NIfTI Interface" begin
         @test slicecode(img) == "Unkown"
         @test sliceduration(img) == 0
-        @test slicestart(img) == 0
-        @test sliceend(img) == 23
-        @test frequencydim(img) == 1
-        @test phasedim(img) == 2
-        @test slicedim(img) == 3
+        @test slicestart(img) == 1
+        if file_name == HDR || file_name == GZIPPED_HDR
+            # These fields aren't written in the HDR example data so we test defaults
+            @test sliceend(img) == 1
+            @test frequencydim(img) == 1
+            @test phasedim(img) == 1
+            @test slicedim(img) == 1
+        else
+            @test sliceend(img) == 23
+            @test frequencydim(img) == 1
+            @test phasedim(img) == 2
+            @test slicedim(img) == 3
+        end
         @test qformcode(img)  == :Scanner_anat
-        @test qform(img)  # TODO
+        # TODO Something is messed up with these values
+        @test qform(img) == (Float32[-1.98, -0.0324275, -0.438552, 2.0],
+                             Float32[0.0324275, 1.99371, -0.355528, 2.0],
+                             Float32[-0.398683, 0.323208, 2.14908, 2.2],
+                             Float32[0.0, 0.0, 0.0, 1.0])
         @test sform(img) == ((-2.00, 0.00,  0.00  117.86),
                              ( 0.00, 1.97, -0.36, -35.72),
                              ( 0.00, 0.32,  2.17,  -7.25))
     end
 
     @testset "ImageFormat Interface" begin
-        @test spatunits(img) == u"mm"
-        @test timeunits(img) == u"ms"
-        @test description(img) == "FSL3.3\0 v2.25 NIfTI-1 Single file format\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+        if file_name == HDR || file_name == GZIPPED_HDR
+            @test spatunits(img) == Unitful.FreeUnits{(),NoDims,nothing}()
+            @test timeunits(img) == u"s"
+            @test description(img) == "FSL5.0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+        else
+            @test spatunits(img) == u"mm"
+            @test timeunits(img) == u"ms"
+            @test description(img) == "FSL3.3\0 v2.25 NIfTI-1 Single file format\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+        end
         @test auxfile(img) == "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
-        @test data_offset(img) == 416
+        # TODO this should be tested on a stream, not an image
+        #@test data_offset(img) == 416
     end
 
     @testset "AbstractArray Interface" begin
@@ -48,8 +67,10 @@ for file_name in (NII, GZIPPED_NII, HDR, GZIPPED_HDR)
         @test length(img) == prod((128, 96, 24, 2))
         @test eltype(img) == Int16
         # @test img[64, 48, 13, 1] == 496 FIXME
-        @test img[64, 48, 12, :] == [265, 266]
-        @test img[69, 56, 13, :] == [502, 521]
+        @test img[65, 49, 13, 1] == 265
+        @test img[65, 49, 13, 2] == 266
+        @test img[70, 57, 14, 1] == 502
+        @test img[70, 57, 14, 2] == 521
     end
 
     @testset "Images Interface" begin
@@ -60,7 +81,7 @@ for file_name in (NII, GZIPPED_NII, HDR, GZIPPED_HDR)
         @test sdims(img) == 3
         @test nimages(img) == 2
 
-        @test ustrip(timeaxis(img)[end]) == 2000000
+        @test ustrip(timeaxis(img)[end]) == 2000
         @test ustrip(timeaxis(img)[1]) == 0
         @test timedim(img) == 4
         #@test indices_spatial(img) == ((1.0:2.0:255.0)*u"mm",
