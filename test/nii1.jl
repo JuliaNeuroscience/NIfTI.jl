40661e18057e4c576daf43cca2d1e9d754978e83
Tokazama <zchristensen7@gmail.com>
2019-09-16 04:42:27 -0400

    The last row of affine in NIfTI should ALWAYS be 0 0 0 1

--- test/nii1.jl
+++ test/nii1.jl
@@ -20,10 +20,10 @@ function nii1_tests(img::AbstractArray)
 
         # dim_info = 57
         @test sliceend(img) == 24
-        @test frequencydim(img) == 2
+        @test freqdim(img) == 2
         @test phasedim(img) == 3
         @test slicedim(img) == 4
-        @test qformcode(img)  == :Scanner_anat
+        @test qformcode(img)  == CoordinateSpace{:scanner}()
         # TODO Something is messed up with these values
         # The NIfTI format tries to normalize the qform to orientation. Therefore, it's not
         # always (rarely) used by other libraries, but we need it for when we interact with
@@ -33,11 +33,11 @@ function nii1_tests(img::AbstractArray)
         @test sform(img) ≈ [ -2.0                    6.714715653593746e-19  9.081024511081715e-18  117.8551025390625
                               6.714715653593746e-19  1.9737114906311035    -0.35552823543548584   -35.72294235229492
                               8.25548088896093e-18   0.3232076168060303     2.171081781387329     -7.248798370361328
-                              0.0                    0.0                    0.0                   -1.0]
+                              0.0                    0.0                    0.0                   1.0]
     end
 
     @testset "ImageFormat Interface" begin
-        @test all(spatunits(img) .== u"mm")
+        @test all(spatialunits(img) .== u"mm")
         @test timeunits(img) == u"s"
         @test description(img) == "FSL3.3\0 v2.25 NIfTI-1 Single file format\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
         @test auxfiles(img) == ["\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"]
@@ -72,7 +72,7 @@ function nii1_tests(img::AbstractArray)
         #                               (1.0:2.0:191.0)*u"mm",
         #                               (1.0:2.1999990940093994:51.59997916221619)*u"mm")
         @test size_spatial(img) == (128, 96, 24)
-        @test spatialorder(img) == (:R2L, :P2A, :I2S)
+        @test spatialorder(img) == (:right_to_left, :posterior_to_anterior, :inferior_to_superior)
     end
 end
 function hdr1_tests(img::AbstractArray)
@@ -82,10 +82,10 @@ function hdr1_tests(img::AbstractArray)
         @test slicestart(img) == 1
         # These fields aren't written in the HDR example data so we test defaults
         @test sliceend(img) == 1
-        @test frequencydim(img) == 1
+        @test freqdim(img) == 1
         @test phasedim(img) == 1
         @test slicedim(img) == 1
-        @test qformcode(img)  == :Scanner_anat
+        @test qformcode(img)  == CoordinateSpace{:scanner}()
         # TODO Something is messed up with these values
         # The NIfTI format tries to normalize the qform to orientation. Therefore, it's not
         # always (rarely) used by other libraries, but we need it for when we interact with
@@ -95,11 +95,11 @@ function hdr1_tests(img::AbstractArray)
         @test sform(img) ≈ [ -2.0                    6.714715653593746e-19  9.081024511081715e-18  117.8551025390625
                               6.714715653593746e-19  1.9737114906311035    -0.35552823543548584   -35.72294235229492
                               8.25548088896093e-18   0.3232076168060303     2.171081781387329     -7.248798370361328
-                              0.0                    0.0                    0.0                   -1.0]
+                              0.0                    0.0                    0.0                   1.0]
     end
 
     @testset "ImageFormat Interface" begin
-        @test all(spatunits(img) .== u"mm")
+        @test all(spatialunits(img) .== u"mm")
         @test timeunits(img) == u"s"
         @test description(img) == "FSL5.0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
         @test auxfiles(img) == ["\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"]
@@ -134,36 +134,36 @@ function hdr1_tests(img::AbstractArray)
         #                               (1.0:2.0:191.0)*u"mm",
         #                               (1.0:2.1999990940093994:51.59997916221619)*u"mm")
         @test size_spatial(img) == (128, 96, 24)
-        @test spatialorder(img) == (:R2L, :P2A, :I2S)
+        @test spatialorder(img) == (:right_to_left, :posterior_to_anterior, :inferior_to_superior)
     end
 end
 
 
 
 @testset ".nii+mmap" begin
-    img = load(NII, mmap=true)
+    img = NIfTI.load(File(format"NII", NII), mmap=true)
     nii1_tests(img)
 end
 
 @testset ".nii.gz" begin
-    img = load(File(format"NII", GZIPPED_NII))
+    img = NIfTI.load(File(format"NII", GZIPPED_NII))
     nii1_tests(img)
 end
 
 @testset "save" begin
-    img = load(File(format"NII", GZIPPED_NII))
+    img = NIfTI.load(File(format"NII", GZIPPED_NII))
     save(TEMPNII_FILE, img)
-    img = load(File(format"NII", TEMPNII_FILE))
+    img = NIfTI.load(File(format"NII", TEMPNII_FILE))
     nii1_tests(img)
 end
 
 @testset ".hdr" begin
-    img = load(File(format"NII", HDR), mmap=true)
+    img = NIfTI.load(File(format"NII", HDR), mmap=true)
     hdr1_tests(img)
 end
 
 @testset ".hdr.gz" begin
-    img = load(File(format"NII", GZIPPED_HDR))
+    img = NIfTI.load(File(format"NII", GZIPPED_HDR))
     hdr1_tests(img)
 end
 
@@ -173,20 +173,20 @@ end
     ARL = joinpath(dirname(@__FILE__), "data/avg152T1_RL_nifti.nii.gz")
 
     # LR
-    img = load(File(format"NII", ALR))
+    img = NIfTI.load(File(format"NII", ALR))
     @test size(img) == (91,109,91)
-    @test spatialorder(img) == (:R2L, :P2A, :I2S)
+    @test spatialorder(img) == (:right_to_left, :posterior_to_anterior, :inferior_to_superior)
 
     # RL
-    img = load(File(format"NII", ARL))
+    img = NIfTI.load(File(format"NII", ARL))
     @test size(img) == (91,109,91)
-    @test spatialorder(img) == (:L2R, :P2A, :I2S)
+    @test spatialorder(img) == (:left_to_right, :posterior_to_anterior, :inferior_to_superior)
 end
 
 
 @testset "Save Array" begin
     save(TEMPNII_FILE, rand(4,4,4))
-    img = load(File(format"NII", TEMPNII_FILE))
+    img = NIfTI.load(File(format"NII", TEMPNII_FILE))
 end
 
 #img2 = permutedims(img, (2,1,3,4))
