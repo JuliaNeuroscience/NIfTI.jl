d9872c1c33c61c9b16026a2804f527a9f64d3af7
korbi <korbinian90@gmail.com>
2019-12-26 11:32:05 +0100

    write to mmapped arrays via mode="r+" in niread()

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,6 +48,16 @@ vol = NIVolume()
 niwrite(TEMP_FILE, vol)
 niread(TEMP_FILE)
 
+# Open mmaped file for reading and writing
+const WRITE = "$(tempname()).nii"
+const VERIFY_WRITE = "$(tempname()).nii"
+cp(NII, WRITE)
+img = niread(WRITE; mmap=true, mode="r+")
+img.raw[1,1,1,1] = 5
+img.raw[:,2,1,1] = ones(size(img)[1])
+cp(WRITE, VERIFY_WRITE)
+@test niread(VERIFY_WRITE)[1,1,1,1] == 5
+@test niread(VERIFY_WRITE)[:,2,1,1] == ones(size(img)[1])
 # Site is currently down TODO: reintroduce this test when site is up
 # Big endian
 # const BE = "$(tempname()).nii"
@@ -55,9 +65,12 @@ niread(TEMP_FILE)
 img = niread("data/avg152T1_LR_nifti.nii.gz")
 @test size(img) == (91,109,91)
 
+GC.gc() # closes mmapped files
 # Clean up
 rm(NII)
 rm(HDR)
 rm(IMG)
 rm(TEMP_FILE)
+rm(WRITE)
+rm(VERIFY_WRITE)
 # rm(BE)
