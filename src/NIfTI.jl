d6af1ec5b59081af2ace71bc8a9e4bc36672b391
Christopher Lin <christopher.lin045@gmail.com>
2022-03-22 16:18:05 -0400

    Update NIfTI.jl

--- src/NIfTI.jl
+++ src/NIfTI.jl
@@ -382,7 +382,7 @@ add1(::Colon) = Colon()
     vox(f::NIVolume, args...,)
 
 Get the value of a voxel from volume `f`, scaled by slope and intercept given in header, with 0-based indexing.
-The length of `args` should be the number of dimensions in `f`.
+`args` are the voxel indices and the length should be the number of dimensions in `f`.
 """
 @inline vox(f::NIVolume, args...,) = getindex(f, map(add1, args)...,)
 
