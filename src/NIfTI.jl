06d4ce372ffc1a3ce31953ecad8273afa6a6c868
Zachary P. Christensen <zchristensen7@gmail.com>
2022-03-24 08:13:32 -0400

    phasdim, freqdim, slicedim support

--- src/NIfTI.jl
+++ src/NIfTI.jl
@@ -1,6 +1,3 @@
-# NIfTI.jl
-# Methods for reading NIfTI MRI files in Julia
-
 module NIfTI
 
 using CodecZlib, Mmap, MappedArrays, TranscodingStreams
@@ -11,101 +8,7 @@ export NIVolume, niread, niwrite, voxel_size, time_step, vox, getaffine, setaffi
 include("parsers.jl")
 include("extensions.jl")
 include("volume.jl")
-
-function define_packed(ty::DataType)
-    packed_offsets = cumsum([sizeof(x) for x in ty.types])
-    sz = pop!(packed_offsets)
-    pushfirst!(packed_offsets, 0)
-
-    @eval begin
-        function Base.read(io::IO, ::Type{$ty})
-            bytes = read!(io, Array{UInt8}(undef, $sz...))
-            hdr = $(Expr(:new, ty, [:(unsafe_load(convert(Ptr{$(ty.types[i])}, pointer(bytes)+$(packed_offsets[i])))) for i = 1:length(packed_offsets)]...,))
-            if hdr.sizeof_hdr == ntoh(Int32(348))
-                return byteswap(hdr), true
-            end
-            hdr, false
-        end
-        function Base.write(io::IO, x::$ty)
-            bytes = UInt8[]
-            for name in fieldnames($ty)
-                append!(bytes, reinterpret(UInt8, [getfield(x,name)]))
-            end
-            write(io, bytes)
-            $sz
-        end
-    end
-    nothing
-end
-
-mutable struct NIfTI1Header
-    sizeof_hdr::Int32
-
-    data_type::NTuple{10,UInt8}
-    db_name::NTuple{18,UInt8}
-    extents::Int32
-    session_error::Int16
-    regular::Int8
-
-    dim_info::Int8
-    dim::NTuple{8,Int16}
-    intent_p1::Float32
-    intent_p2::Float32
-    intent_p3::Float32
-    intent_code::Int16
-    datatype::Int16
-    bitpix::Int16
-    slice_start::Int16
-    pixdim::NTuple{8,Float32}
-    vox_offset::Float32
-    scl_slope::Float32
-    scl_inter::Float32
-    slice_end::Int16
-    slice_code::Int8
-    xyzt_units::Int8
-    cal_max::Float32
-    cal_min::Float32
-    slice_duration::Float32
-    toffset::Float32
-
-    glmax::Int32
-    glmin::Int32
-
-    descrip::NTuple{80,UInt8}
-    aux_file::NTuple{24,UInt8}
-
-    qform_code::Int16
-    sform_code::Int16
-    quatern_b::Float32
-    quatern_c::Float32
-    quatern_d::Float32
-    qoffset_x::Float32
-    qoffset_y::Float32
-    qoffset_z::Float32
-
-    srow_x::NTuple{4,Float32}
-    srow_y::NTuple{4,Float32}
-    srow_z::NTuple{4,Float32}
-
-    intent_name::NTuple{16,UInt8}
-
-    magic::NTuple{4,UInt8}
-end
-define_packed(NIfTI1Header)
-
-# byteswapping
-
-function byteswap(hdr::NIfTI1Header)
-    for fn in fieldnames(typeof(hdr))
-        val = getfield(hdr, fn)
-        if isa(val, Number) && sizeof(val) > 1
-            setfield!(hdr, fn, ntoh(val))
-        elseif isa(val, NTuple) && sizeof(eltype(val)) > 1
-            setfield!(hdr, fn, map(ntoh, val))
-        end
-    end
-    hdr
-end
+include("headers.jl")
 
 """
     NIVolume{T<:Number,N,R} <: AbstractArray{T,N}
@@ -135,6 +38,8 @@ NIVolume(header::NIfTI1Header, extensions::Vector{NIfTIExtension}, raw::Abstract
 NIVolume(header::NIfTI1Header, raw::AbstractArray{Bool,N}) where {N} =
     NIVolume{Bool,N,typeof(raw)}(header, NIfTIExtension[], raw)
 
+
+header(x::NIVolume) = getfield(x, :header)
 
 include("coordinates.jl")
 
@@ -393,4 +298,3 @@ length(f::NIVolume) = length(f.raw)
 lastindex(f::NIVolume) = lastindex(f.raw)
 
 end
-
